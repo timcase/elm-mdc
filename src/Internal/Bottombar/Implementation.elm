06682533ca3f96c87edf076795ec456435d77e43
Tim Case <tim@2drops.net>
2018-07-28 14:35:42 +0700

    Change header to footer

--- src/Internal/Bottombar/Implementation.elm
+++ src/Internal/Bottombar/Implementation.elm
@@ -215,7 +215,7 @@ bottombar lift model options sections =
             Maybe.withDefault 0 model.styleTop
     in
         Options.apply summary
-            Html.header
+            Html.footer
             [ cs "mdc-bottombar"
             , when config.dense (cs cssClasses.dense)
             , when config.fixed (cs cssClasses.fixed)
